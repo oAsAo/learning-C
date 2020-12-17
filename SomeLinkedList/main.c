fddafcc1fa84b37443eaf78e3e008b894c904f4f
Sergey <SerMorUfa@gmail.com>
2020-12-17 23:55:22 +0500

    Wrong file on last commit, replaced

--- SomeLinkedList/main.c
+++ SomeLinkedList/main.c
@@ -1,51 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// defining point struct(class)
-struct point{
-	float x;
-	float y;
+// linked list node
+// a -> b -> c ... -> d -> NULL
+struct node {
+	int this_;
+	struct node *next;
 };
 
-int main(int argc, char **argv)
-{
-	struct point *pointer = 0;
-	
-	/* defining variable(object)
-	   of type point - p*/
-	struct point p;
-	p.x = 3.14; p.y = 2.78;
-	struct point p2;
-	p2.x = 23.14; p2.y = 23.78;
-	printf("%f\n", p.x);
-	printf("%f\n", p2.x);
-	
-	// sizeof point
-	printf("%ld\n", sizeof(struct point));
-	printf("%ld\n", sizeof(p));
-	
-	// defining a single struct(object)
-	struct{
-		float x;
-		float y;
-		float z;
-	} p3d;
-	p3d.x = 1; p3d.y = 2; p3d.z = 3;
-	printf("%f\n", p3d.y);
-	
-	// dynamically allocated struct
-	pointer = malloc(sizeof(struct point));
-	
-	/* 2 ways to reference it's members
-	   "->" is apparently recomended */
-	(*pointer).x = 42.0;
-	pointer -> y = 12.5;
-	printf("%f\n", pointer->x);
-	printf("%f\n", (*pointer).y);
-	
-	free(pointer);
-	
-	// Note: typedef, union, enum... also exist
+struct node new_list(int x);
+void print_list(struct node current);
+void append_to_list(struct node *current, int i, int x);
+int pop_list(struct node *current, int i);
 
+int main(int argc, char **argv) {
+	struct node A = new_list(2);
+	print_list(A);
+	append_to_list(&A, 0, 3);
+	print_list(A);
+	append_to_list(&A, 1, 5);
+	print_list(A);
+	append_to_list(&A, 1, 7);
+	print_list(A);
+	
+	printf("%d\n", pop_list(&A, 1));
+	print_list(A);
+	printf("%d\n", pop_list(&A, 2));
+	print_list(A);
+	printf("%d\n", pop_list(&A, 1));
+	print_list(A);
+	// can't pop last value left
+	printf("%d\n", A.this_);
+	
 	return 0;
+}
+
+// creates and returns a new head, O(1)
+struct node new_list(int x) {
+	struct node head;
+	head.this_ = x; head.next = NULL;
+	return head;
+}
+
+// prints the list when passed head O(n)
+void print_list(struct node current){
+	printf("[");
+	while (current.next) {
+		printf("%d, ", current.this_);
+		current = *current.next;
+	}
+	printf("%d]\n", current.this_);
+}
+
+// Adds a value to some place in the list, O(n)
+void append_to_list(struct node *current, int i, int x){
+	struct node *after = NULL;
+	while ((current -> next) && (i != 0)) {
+		current = current -> next;
+		i--;
+	}
+	
+	if (current -> next){
+		after = current -> next;
+	}
+	current -> next = malloc(sizeof(struct node));
+	current = current -> next;
+	
+	current -> this_ = x;
+	if (after) {
+		current -> next = after;
+	}
+}
+
+// Removes a value from some place in the list, O(n)
+int pop_list(struct node *current, int i){
+	int value = 0;
+	struct node *after = NULL;
+	
+	while (((current -> next) -> next) && (i != 1)) {
+		current = current -> next;
+		i--;
+	}
+	if ((current -> next) -> next) {
+		after = ((current -> next) -> next);
+	}
+	value = (current -> next) -> this_;
+	
+	free(current -> next);
+	if (after) {
+		current -> next = after;
+	} else {
+		current -> next = NULL;
+	}
+	
+	
+	
+	return value;
 }
